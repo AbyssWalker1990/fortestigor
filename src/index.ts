69bca439ff02bd3c5acb620546be085ececbfd7d
Ihor Kopieichykov <igor129k02@gmail.com>
2023-08-19 16:11:46 +0300

    feat: add class with POST & GET requests handler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import { AIClient } from "./client/AIClient";
 
+
+const app = new AIClient({
+	url: "http://localhost:3001/api/v1/faq",
+	method: "GET",
+});
+(async () => {
+    const {response} = await app.invoke("Gbc.y");
+    console.log("response: ", response)
+})()
+const app2 = new AIClient({
+	url: "localhost:3001/api/v1/faq",
+	method: "POST",
+});
+(async () => {
+    const {response} = await app2.invoke("Gbc.y");
+    console.log("response: ", response)
+})()
+
 export default AIClient;
